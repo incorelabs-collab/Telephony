4f031c022385eae43c01361fdfc96f1bf94d6c16
Neelabh Pandey <incore.labs.np@gmail.com>
2014-12-19 07:48:23 +0530

    Added list.js as well as search and sort functionality

--- js/index.js
+++ js/index.js
@@ -9,21 +9,28 @@ var app = {
                 //console.log(contacts.length + ' contacts found');
                 //$('#content').append(contacts.length + ' contacts found.<br/>');
                 for(var i = 0; i < contacts.length; i++) {
-                    var concatContact = "<div class='row outercard'><div class='col-xs-3 left-column'><img src='content://com.android.contacts/contacts/"+contacts[i].id+"/photo' onerror='app.imgError(this)' width='64px' height='64px'/></div><div class='col-xs-9 right-column'><ul class='list-group'><li class='list-group-item'><b>"+contacts[i].displayName+"</b></li>";
+                    var concatContact = "<li><div class='row outercard'><div class='col-xs-3 left-column'><img src='content://com.android.contacts/contacts/"+contacts[i].id+"/photo' onerror='app.imgError(this)' width='64px' height='64px'/></div><div class='col-xs-9 right-column'><ul class='list-group'><li class='list-group-item'><b><span class='name'>"+contacts[i].displayName+"</span></b></li>";
                     //console.log(contacts[i].id + " - " + contacts[i].displayName);
                     //$('#content').append(contacts[i].id + " - " + contacts[i].displayName + '<br/>');
                     for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
                         var phone = contacts[i].phoneNumbers[j];
                         //console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-                        concatContact += "<li class='list-group-item'><b>"+phone.type+" :</b><br/>"+phone.number+"<input type='checkbox' name='my_number' value='"+phone.normalizedNumber+"' class='pull-right'/></li>";
+                        concatContact += "<li class='list-group-item'><b>"+phone.type+" :</b><br/><span class='phone'>"+phone.normalizedNumber+"</span><input type='checkbox' name='my_number' value='"+phone.normalizedNumber+"' class='pull-right'/></li>";
                         //$('#content').append("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")<br/>");
                     }
-                    concatContact += "</ul></div></div>";
-                    $('#content').append(concatContact);
+                    concatContact += "</ul></div></div></li>";
+                    $('#contactList').append(concatContact);
                     concatContact = "";
                 }
+                var options = {
+                    valueNames: [ 'name', 'phone' ],
+                    indexAsync: true
+                };
+                var contactList = new List('content', options);
+                contactList.sort(['name'],{order:"asc"});
             }, function(error) {
                 //console.error(error);
+                $('#content').empty();
                 $('#content').append(error);
             });
         } else {
@@ -50,7 +57,7 @@ var app = {
         //options.desiredFields = [navigator.contacts.fieldType.id];
         //var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
         navigator.contacts.find(fields, app.onContactSuccess, app.onContactError, options);
-    },
+        },
     onContactSuccess: function(contacts) {
         $('#content').append(JSON.stringify(contacts));
         $('#content').append('<br/><br/>');
